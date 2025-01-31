1ae997a6e759a34392bfaa90eb4fd01df2d20f74
roman alekseev <roma.alekseev.90@bk.ru>
2025-01-31 16:02:18 +0300

    Добавлен полифил для NodeList.prototype.forEach

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+// Полифилл для метода forEach для NodeList
+if (window.NodeList && !NodeList.prototype.forEach) {
+  NodeList.prototype.forEach = function (callback, thisArg) {
+    thisArg = thisArg || window;
+    for (var i = 0; i < this.length; i++) {
+      callback.call(thisArg, this[i], i, this);
+    }
+  };
+}
+
 document.querySelectorAll(".dropdown").forEach((dropDownWrapper) => {
   const dropDownButton = dropDownWrapper.querySelector(".dropdown__button");
   const dropDownList = dropDownWrapper.querySelector(".dropdown__list");
